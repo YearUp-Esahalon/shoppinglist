b5687b3e4fd165f6ac2ea1118b4288993c81f272
Elan Sahalon <Esahalon1996@gmail.com>
2024-09-25 15:37:08 -0400

    files from java

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+
+    public class ShoppingList {
+
+
+        public static void main(String[] args)
+        {
+            System.out.println("Shopping List");
+            //System.out.println
+        }
+    }
+
